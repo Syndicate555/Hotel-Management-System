847b0478700f2208c5755ad5da56b057e885799f
Saffat Aziz <saziz028@uottawa.ca>
2021-03-28 21:15:16 -0400

    Port moved to .env file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 const pool = require("./db")
 const cors = require("cors")
 require('dotenv').config
