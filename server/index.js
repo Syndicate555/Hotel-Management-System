ffab330e61c72916737b008886aca7c18e15f061
Saffat Aziz <saziz028@uottawa.ca>
2021-03-29 20:22:05 -0400

    middleware added

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const app = express()
-
+const path = require('path')
 const pool = require("./db")
 const morgan = require('morgan')
 const cors = require("cors")
+const bodyParser = require('body-parser')
 require('dotenv').config
-app.use(morgan('combined'))
+app.use(morgan('dev'))
+app.use(express.static('./public'))
 
 // Middlewares
 app.use(cors())
 app.use(express.json()) // Lets us access req.body
+app.use(express.urlencoded({ extended: false }))
 
 //Routes//
 
@@ -19,10 +22,9 @@ app.post("/todos", async (req, res) => {
  try {
   const { description, fname, date } = req.body;
 
-  console.log(description)
-  const newTodo = await pool.query("INSERT INTO todos.todo (description,fname,date_entry) VALUES($1,$2,$3)", [ description, fname, date ])
-  console.log(description)
-  res.json(newTodo)
+  const newTodo = await pool.query("UPDATE todos.todo SET fname = $1 WHERE todo_id = 1", [ fname ])
+  console.log(fname)
+
 
 
  } catch (error) {
