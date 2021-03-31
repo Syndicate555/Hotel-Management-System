b00b5ab1439c2b6b4e0fdc0899c149e252e4b5ad
Saffat Aziz <saziz028@uottawa.ca>
2021-03-30 23:12:03 -0400

    Customer Registration works

--- client/src/components/WalkinBooking.js
+++ client/src/components/WalkinBooking.js
@@ -7,6 +7,36 @@ const WalkinBooking = () => {
  const [ room, setRoom ] = useState(0)
  const [ start, setStart ] = useState(0)
  const [ end, setEnd ] = useState(0)
+ const [ city, setCity ] = useState("")
+ const [ country, setCountry ] = useState("")
+ const onSubmitForm = async e => {
+  e.preventDefault();
+  try {
+   const body = {
+    "sin": sin,
+    "fname": fname,
+    "lname": lname,
+    "city": city,
+    "country": country,
+    "room": room,
+    "start": start,
+    "end": end
+   };
+   const response = await fetch("http://localhost:3000/customer-register", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body)
+   });
+   console.log(body)
+
+   window.location = "/";
+  } catch (err) {
+   console.error(err.message);
+
+  }
+ };
+
+
  return (
   <div className="container" style={{ paddingTop: 50 }}>
    <div className="card shadow-lg o-hidden border-0 my-5">
@@ -15,20 +45,25 @@ const WalkinBooking = () => {
       <div className="col-lg-5 d-none d-lg-flex" >
        <img className="img-fluid" src={`${process.env.PUBLIC_URL}/img/4.jpg`} alt='register_image' style={{ objectFit: "cover", height: 600 }} />
       </div>
-      <div className="col-lg-7" style={{ paddingTop: "30px" }}>
+      <div className="col-lg-7" >
        <div className="p-5" >
         <div className="text-center">
          <h4 className="text-dark mb-4">Customer Registration - Walk In</h4>
 
         </div>
-        <form className="user" >
+        <form className="user" onSubmit={onSubmitForm} >
          <div className="form-group row">
-          <div className="col-sm-6 mb-3 mb-sm-0"><input className="form-control form-control-user" type="text" id="exampleFirstName" placeholder="First Name" name="first_name" /></div>
-          <div className="col-sm-6"><input className="form-control form-control-user" type="text" id="exampleFirstName" placeholder="Last Name" name="last_name" /></div>
+          <div className="col-sm-6 mb-3 mb-sm-0"><input className="form-control form-control-user" type="text" placeholder="First Name" onChange={e => setFname(e.target.value)} /></div>
+          <div className="col-sm-6"><input className="form-control form-control-user" type="text" placeholder="Last Name" onChange={e => setLname(e.target.value)} /></div>
          </div>
          <div className="form-group" ><input className="form-control form-control-user" type="email" placeholder="Your Business Email" required /></div>
-         <div className="form-group" ><input className="form-control form-control-user" type="number" placeholder="Your Sin Number" required /></div>
-         <div className="form-group" ><input className="form-control form-control-user" type="number" placeholder="Room Number" required /></div>
+         <div className="form-group" ><input className="form-control form-control-user" type="number" placeholder="Your Sin Number" onChange={e => setSin(e.target.value)} required /></div>
+         <div className="form-group" ><input className="form-control form-control-user" type="number" placeholder="Room Number" onChange={e => setRoom(e.target.value)} required /></div>
+         <div className="form-group row">
+          <div className="col-sm-6 mb-3 mb-sm-0"><input className="form-control form-control-user" type="text" placeholder="City" onChange={e => setCity(e.target.value)} /></div>
+          <div className="col-sm-6"><input className="form-control form-control-user" type="text" placeholder="Country" onChange={e => setCountry(e.target.value)} /></div>
+         </div>
+
          <div className="form-group row">
           <label className="col-sm-6 mb-3 mb-sm-0" htmlFor="">Check In Date</label>
           <div className="col-sm-6 mb-3 mb-sm-0"><input className="form-control form-control-user" defaultValue="2018-07-22" type="date" onChange={e => setStart(e.target.value)} /></div>
