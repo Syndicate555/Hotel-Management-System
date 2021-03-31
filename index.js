298c9538626250039e22b27cb592345868838d18
Saffat Aziz <saziz028@uottawa.ca>
2021-03-31 04:54:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -103,13 +103,13 @@ app.listen(PORT, () => {
 //   console.log(error);
 //  })
 
-const getRooms = async () => {
- try {
-  const response = await fetch("http://localhost:3000/rooms");
-  const jsonData = await response.json();
+// const getRooms = async () => {
+//  try {
+//   const response = await fetch("http://localhost:3000/rooms");
+//   const jsonData = await response.json();
 
-  console.log(jsonData)
- } catch (err) {
-  console.error(err.message);
- }
-}
+//   console.log(jsonData)
+//  } catch (err) {
+//   console.error(err.message);
+//  }
+// }
