58b1e8d8f203287916844015b3024fa0dc1d78b4
Saffat Aziz <saziz028@uottawa.ca>
2021-04-02 08:14:11 -0400

    Update CustomerBooking.js

--- client/src/components/CustomerBooking.js
+++ client/src/components/CustomerBooking.js
@@ -25,7 +25,62 @@ const ModalWrapper = styled.div`
   z-index: 10;
   border-radius: 10px;
 `;
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.8;
+  color: #141414;
+  p {
+    margin-bottom: 1rem;
+  }
+  button {
+    padding: 10px 24px;
+    background: #141414;
+    color: #fff;
+    border: none;
+  }
+`;
+
+const CloseModalButton = styled(MdClose)`
+  cursor: pointer;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 32px;
+  height: 32px;
+  padding: 0;
+  z-index: 10;
+`;
 const CustomerBooking = ({ showModal, setShowModal }) => {
+ const modalRef = useRef();
+
+ const animation = useSpring({
+  config: {
+   duration: 250
+  },
+  opacity: showModal ? 1 : 0,
+  transform: showModal ? `translateY(0%)` : `translateY(-100%)`
+ });
+
+ const closeModal = e => {
+  if (modalRef.current === e.target) {
+   setShowModal(false);
+  }
+ };
+
+ const keyPress = useCallback(
+  e => {
+   if (e.key === 'Escape' && showModal) {
+    setShowModal(false);
+    console.log('I pressed');
+   }
+  },
+  [ setShowModal, showModal ]
+ );
+
  return (
   <>
    {showModal ? <div>Test</div> : null}
